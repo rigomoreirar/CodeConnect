527cc225d00904aebcfdc73e8793c863ba080ec2
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-07-25 19:57:10 -0600

    Modified nginx.conf and axios instance

--- backend/frontend/src/utils/Axios.js
+++ backend/frontend/src/utils/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const environment = "production";
 const instance = axios.create({
-    baseURL: environment === "development" ? "http://localhost:8000" : "/"
+    baseURL: environment === "development" ? "http://localhost:8000/api" : "/api"
 });
 
 export default instance;
