147e0e0903183c1612a83db4047e801e9d09e99c
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-07-25 20:45:26 -0600

    Fixing deployment

--- backend/forum/settings.py
+++ backend/forum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',  # Comment out this line
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- backend/frontend/src/utils/Axios.js
+++ backend/frontend/src/utils/Axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 const environment = "production";
 const instance = axios.create({
-    baseURL: environment === "development" ? "http://localhost:8000/api" : "/api",
-    headers: {
-        'X-CSRFToken': Cookies.get('csrftoken')
-    }
+    baseURL: environment === "development" ? "http://localhost:8000/api" : "/api"
 });
 
 export default instance;
