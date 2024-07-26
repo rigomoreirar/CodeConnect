a54d2c09efe3e0053c421b021aa9805af4db99ff
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-07-26 13:08:17 -0600

    Modified file structure

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -8,9 +8,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.urls")),  # Ensure API paths start with /api/
+    path('api/', include("api.urls")),
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # Serve React app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
