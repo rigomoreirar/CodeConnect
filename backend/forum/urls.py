0ea8da1033a2121c072c31146634069e13b68bfb
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-08-02 15:48:46 -0600

    Modified static react serve files on backend

--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # Serve React app
+    # re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # Serve React app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
