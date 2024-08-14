dbd92a4d7ad7b3034b2cdfa40a6c66ebd04a574e
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-08-13 21:55:22 -0600

    Modified action comments and nginx conf

--- backend/frontend/src/actions/actionComments.js
+++ backend/frontend/src/actions/actionComments.js
@@ -1,5 +1,5 @@
-import axios from '../utils/axios';
-import endpoints from '../utils/endpoints';
+import axios from "../utils/axios";
+import endpoints from "../utils/endpoints";
 
 export const addComment = async (postId, profileId, content) => {
     const response = await axios.post(endpoints.interactions.addComment, {
@@ -12,16 +12,18 @@ export const addComment = async (postId, profileId, content) => {
 
 export const deleteComment = async (commentId, userId) => {
     const response = await axios.post(endpoints.interactions.deleteComment, {
-        comment_id: commentId,
-        user_id: userId,
+        comment_id: String(commentId),
+        user_id: String(userId),
     });
     return response.data;
 };
 
 export const fetchProfilePicture = async (username) => {
-    const response = await axios.get(endpoints.profile.getProfilePictureByUsername(username), {
-        responseType: 'blob',
-    });
+    const response = await axios.get(
+        endpoints.profile.getProfilePictureByUsername(username),
+        {
+            responseType: "blob",
+        }
+    );
     return URL.createObjectURL(response.data);
 };
-
