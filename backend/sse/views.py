2b51c6d1d65f3d61d55adb092b9b803a06f990b3
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-08-19 23:12:59 -0600

    Changes made

--- backend/sse/views.py
+++ backend/sse/views.py
@@ -10,7 +10,7 @@ def sse_view(request):
         while True:
             # You would replace this with your actual logic to detect changes
             # Here, we're simulating a change notification every 10 seconds
-            time.sleep(1)
+            time.sleep(4)
             yield 'data: {}\n\n'.format(json.dumps({'message': 'refetch', 'route': 'get-all-data'}))
 
     return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
